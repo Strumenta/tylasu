65444989936e63c31f9ece82d46391141bad795d
Alessio Stalla <alessiostalla@gmail.com>
2021-03-10 18:42:12 +0100

    Ecore import (wip)

--- tests/ecore.test.ts
+++ tests/ecore.test.ts
@@ -2,6 +2,9 @@ import {expect} from "chai";
 
 import {fromEObject, registerECoreModel, toEObject} from "../src/interop/ecore";
 import {SomeNode, SomeNodeInPackage} from "./nodes";
+import * as Ecore from "ecore/dist/ecore";
+import * as fs from "fs";
+import {EPackage} from "ecore";
 
 describe('Ecore metamodel', function() {
     it("default package",
@@ -24,9 +27,30 @@ describe('Ecore metamodel', function() {
             expect(eClass.get('eStructuralFeatures').at(0).get("name")).to.equal("a");
             expect(eClass.get('eStructuralFeatures').at(1).get("name")).to.equal("someNode");
         });
+    it("importing",
+    function () {
+            const resourceSet = Ecore.ResourceSet.create();
+            const resource = resourceSet.create({ uri: 'file:data/simplemm.json' });
+            const buffer = fs.readFileSync("tests/data/simplemm.json");
+            resource.load(buffer.toString(), (r, e) => {
+                expect(r).not.to.be.null;
+                expect(r).not.to.be.undefined;
+                const ePackage = r.get("contents").at(0);
+                if(!ePackage.get("nsURI")) {
+                    ePackage.set("nsURI", "");
+                }
+                Ecore.EPackage.Registry.register(ePackage);
+                const buffer = fs.readFileSync("tests/data/simplem.json");
+                Ecore.JSON.parse(r, buffer.toString());
+                const model = r.get("contents").at(1);
+                expect(model).not.to.be.null;
+                expect(model).not.to.be.undefined;
+                expect(model.eClass?.get("name")).to.equal("CompilationUnit");
+            });
+    });
 });
 
-describe('Model generation', function() {
+describe('Model generation and import', function() {
     it("simple EObject creation",
         function () {
             let node = new SomeNodeInPackage("aaa");
