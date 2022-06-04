78a4a13737e38a95b5e62c047fe33ee0d680073f
Federico Tomassetti <federico@tomassetti.me>
2022-06-04 08:40:18 +0200

    initial NodeData loader

--- tests/transformation_trace.test.ts
+++ tests/transformation_trace.test.ts
@@ -1,11 +1,17 @@
 import {expect} from "chai";
 import * as fs from "fs";
+import {loadTranspilationTraceFromJSON, TranspilationTrace} from "../src/transformation_trace";
 
 describe('Transformation traces', function() {
     it("Can load transformation trace produced by Kolasu",
         function () {
             const text = fs.readFileSync('tests/data/rpgtojava-transpilation-example.json', 'utf8')
-            const data = JSON.parse(text)
-            console.log(data)
+            const trace : TranspilationTrace = loadTranspilationTraceFromJSON(text);
+            expect(trace.sourceAST.type).to.equal("com.strumenta.rpgparser.model.CompilationUnit");
+            expect(trace.sourceAST.id).to.equal("src-1");
+            expect(trace.sourceAST.destination.id).to.equal("target-1");
+            expect(trace.sourceAST.destination.type).to.equal("com.strumenta.javaast.JCompilationUnit");
+            expect(trace.sourceAST.position).to.equal({start:{line:1,column:0}, end:{line:32, column:30}});
+            expect(trace.sourceAST.dataDefinitions.length).to.equal(5);
         });
 });
