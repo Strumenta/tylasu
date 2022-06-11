bb757e2a6d3aa0ee9888944bbeace4ba01b7c292
Federico Tomassetti <federico@tomassetti.me>
2022-06-11 13:45:53 +0200

    add "Can load eType for all references in Java metamodel"

--- tests/transformation_trace.test.ts
+++ tests/transformation_trace.test.ts
@@ -5,19 +5,31 @@ import * as Ecore from "ecore/dist/ecore";
 import {KOLASU_TRANSPILATION_URI_V1, TRANSPILATION_EPACKAGE} from "../src/interop/transpilation_package";
 
 describe('Transformation traces', function() {
+        it("Can load eType for all references in Java metamodel",
+            function () {
+                    this.timeout(0);
+                    const resourceSet = Ecore.ResourceSet.create();
+                    Ecore.EPackage.Registry.register(THE_AST_EPACKAGE)
+                    Ecore.EPackage.Registry.register(TRANSPILATION_EPACKAGE)
+                    const rpgMetamodelsResource = resourceSet.create({uri: 'file:/tests/data/total-bench/rpg-metamodels.json'})
+                    const javaPackages = loadEPackages(JSON.parse(fs.readFileSync("tests/data/total-bench/java-metamodels.json").toString()),
+                        rpgMetamodelsResource);
+                    expect(rpgMetamodelsResource.eContents().length).to.eql(1);
+                    const javaast = rpgMetamodelsResource.eContents()[0];
+                    expect(javaast.eClass.get("name")).to.eql("EPackage");
+                    expect(javaast.eContents().length).to.eql(31);
+                    const jCompilationUnit = javaast.eContents()[30];
+                    expect(jCompilationUnit.eClass.get("name")).to.eql("EClass");
+                    expect(jCompilationUnit.get("name")).to.eql("JCompilationUnit");
+                    expect(jCompilationUnit.eContents().length).to.eql(1);
+                    const declarations = jCompilationUnit.eContents()[0];
+                    expect(declarations.eClass.get("name")).to.eql("EReference");
+                    expect(declarations.get("name")).to.eql("declarations");
+                    expect(declarations.get("eType").get("name")).to.eql("JClassDeclaration");
+            }
+        );
     it("Can load transformation trace produced by Kolasu",
         function () {
-// <<<<<<< HEAD
-//             const text = fs.readFileSync('tests/data/transpilation/rpgtojava-transpilation-example.json', 'utf8')
-//             const trace : TranspilationTrace = TranspilationTrace.loadFromJSON(text);
-//             expect(trace.sourceAST.type).to.equal("com.strumenta.rpgparser.model.CompilationUnit");
-//             expect(trace.sourceAST.id).to.equal("src-1");
-//             expect(trace.getNodeById("target-3").attributes.name).to.equal("NBR");
-//             expect(trace.sourceAST.destination.id).to.equal("target-1");
-//             expect(trace.sourceAST.destination.type).to.equal("com.strumenta.javaast.JCompilationUnit");
-//             expect(trace.sourceAST.position).to.eql({start:{line:1,column:0}, end:{line:32, column:30}});
-//             expect(trace.sourceAST.children.dataDefinitions.length).to.equal(5);
-// =======
             this.timeout(0);
             const resourceSet = Ecore.ResourceSet.create();
             Ecore.EPackage.Registry.register(THE_AST_EPACKAGE)
