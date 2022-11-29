300e1124bdc757acd9798915f49632e5d5c53089
fabio <fabio.moriero@strumenta.com>
2022-11-29 17:28:51 +0100

    Optional argument

--- src/naming.ts
+++ src/naming.ts
@@ -17,7 +17,7 @@ export interface Named extends PossiblyNamed {
  * A reference associated by using a name.
  */
 export class ReferenceByName<N extends PossiblyNamed> {
-    constructor(public readonly name: string, public referred: N | undefined = undefined) {}
+    constructor(public readonly name: string, public referred?: N) {}
 
     toString(): string {
         if (this.referred == null) {
