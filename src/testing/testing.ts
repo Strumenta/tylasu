76874f61815a86f540d246a17d53b11e90c6a77d
fabio <fabio.moriero@strumenta.com>
2022-12-12 18:59:07 +0100

    [WIP] assertASTsAreEqual

---
+++ src/testing/testing.ts
@@ -0,0 +1,63 @@
+import {Node} from "../model/model";
+import {fail} from "assert";
+import {strict as assert} from "assert";
+import {expect} from "chai";
+
+export function assertASTsAreEqual(
+    expected: Node,
+    actual: Node,
+    context = "<root>",
+    considerPosition = false
+) {
+    if (areSameType(expected, actual)) {
+        if (considerPosition) { // TODO: create unit test for this case
+            assert(expected.position == actual.position, `${context}.position`)
+        }
+        expected.properties.forEach(expectedProperty => {
+            const actualPropValue = actual.properties.find(p => p.name == expectedProperty.name)!.value;
+            const expectedPropValue = expectedProperty.value;
+
+            if (actualPropValue instanceof Node && expectedPropValue instanceof Node) {
+                assertASTsAreEqual(
+                    actualPropValue as Node,
+                    expectedPropValue as Node,
+                    `${context}.${expectedProperty.name}`,
+                    considerPosition
+                );
+            }
+            else if (Array.isArray(actualPropValue) && Array.isArray(expectedPropValue)) {
+                assert(actualPropValue.length == expectedPropValue.length,
+                    `${context}, property ${expectedProperty.name} has length ${expectedPropValue.length}, but found ${actualPropValue.length}`);
+                for (let i = 0; i < expectedPropValue.length; i++) {
+                    const expectedElement = expectedPropValue[i];
+                    const actualElement = actualPropValue[i];
+
+                    if (expectedElement instanceof Node && actualElement instanceof Node) {
+                        assertASTsAreEqual(
+                            expectedElement as Node,
+                            actualElement as Node,
+                            `${context}.${expectedProperty.name}[${i}]`,
+                            considerPosition
+                        );
+                    }
+                    else if (typeof expectedElement != "object" && typeof actualElement != "object") {
+                        assert(expectedElement == actualElement,
+                            `${context}, property ${expectedProperty.name}[${i}] is ${expectedPropValue[i]}, but found ${actualPropValue[i]}`);
+                    }
+                }
+            }
+            else {
+                assert(
+                    expectedPropValue == actualPropValue,
+                    `${context}, comparing property ${expectedProperty.name}`);
+            }
+        });
+    }
+    else {
+        fail(`${context}: nodes are not of the same type`);
+    }
+}
+
+function areSameType(a, b) : boolean {
+    return typeof a == typeof b; // FIXME: this won't check they are of the same types other than "object"s
+}
