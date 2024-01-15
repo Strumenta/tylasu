36d310ffdde3fb7e820ff733f6b813ef8915af1e
Alessio Stalla <alessiostalla@gmail.com>
2024-01-15 17:04:14 +0100

    Port missing functions `walkAncestors` and `findAncestorOfType` from Kolasu

---
+++ tests/traversing/structurally.test.ts
@@ -0,0 +1,25 @@
+import {Box} from "../nodes";
+import {expect} from "chai";
+import {assignParents} from "../../src";
+import {printSequence, testCase} from "../traversing.test";
+
+describe('Traversing structurally', function() {
+    it("depth-first",
+        function () {
+            const result = "root, first, 1, 2, big, small, 3, 4, 5, 6";
+            expect(printSequence(testCase.walk())).to.equal(result);
+        });
+    it("ancestors",
+        function () {
+            assignParents(testCase);
+            const item4 = ((testCase.contents[2] as Box).contents[0] as Box).contents[1];
+            const result = printSequence(item4.walkAncestors());
+            expect(result).to.equal("small, big, root");
+        });
+    it("descendants",
+        function () {
+            const result = "first, 1, 2, big, small, 3, 4, 5, 6";
+            expect(printSequence(testCase.walkDescendants())).to.equal(result);
+        });
+});
+
