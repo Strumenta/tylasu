99d8c9c820798f27a157d9ec43165de4812a677a
Mochitto <mochittodeveloper@gmail.com>
2024-07-16 10:51:41 +0200

    Improve dev-x with `prettier` and `husky` hooks
    Resolve #75
    To have consistent formatting across the project and facilitate
    maintenance and contributions I've added `prettier` (with an
    *import-statements* sorting plugin to improve readability) and
    *git-hooks* support with `husky`.
    
    *Git hooks* with `husky` formats code and blocks commits until all the
    desired conditions are met.
    The current configuration formats, lints and tests code, before allowing
    users to commit changes.
    
    Further improvements to the dev-x and contributions could be added by
    making the repo [commitizen friendly](https://github.com/commitizen/cz-cli)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,17 @@
 /**@type {import('eslint').Linter.Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
-	root: true,
-	parser: '@typescript-eslint/parser',
-	plugins: [
-		'@typescript-eslint',
-	],
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-	],
-	rules: {
-		'@typescript-eslint/explicit-module-boundary-types': "off",
-		'@typescript-eslint/no-explicit-any': "off",
-		'@typescript-eslint/no-non-null-assertion': "off"
-	}
+    root: true,
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+    ],
+    rules: {
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-non-null-assertion": "off",
+    },
 };
